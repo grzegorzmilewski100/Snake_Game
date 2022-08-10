1c1bdc5e02484b5934a7cb97a040f99069c46827
Grzegorz Milewski <grzegorzmilewski@MacBook-Air-Grzegorz.local>
2022-08-10 11:08:17 +0200

    Creating main file with the screen

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen,Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+
+
+
+screen.exitonclick()
