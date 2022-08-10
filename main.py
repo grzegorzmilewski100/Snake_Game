ebba47cc629b6bc6e0aae2e9018cca203f8b3457
Grzegorz Milewski <grzegorzmilewski@MacBook-Air-Grzegorz.local>
2022-08-10 11:20:52 +0200

    Creating the body of the snake

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+starting_position = [(0,0), (-20,0), (-40,0)]
 
-
-
-
-
-
-
-screen.exitonclick()
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
